68d0865e5ce08e9461ded24eb48c8f6b5433662f
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-05-19 14:22:49 +0900

    add project

--- ios-foundation-dateformatter/ViewController.swift
+++ ios-foundation-dateformatter/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
+let dateFormatter = DateFormatter()
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setDateFormat()
+        setLocale()
+        testDateFormatterStyle()
+        setTimeStyle()
     }
+    
+    private func setDateFormat(){
+    
+        dateFormatter.dateFormat = "yy/MM/dd HH:mm:ss"
+    }
+    
+    private func setLocale(){
+        
+        dateFormatter.locale = Locale(identifier: "ja_JP")
+    }
+    
+    private func testDateFormatterStyle(){
+        
+        dateFormatter.dateStyle = .full
+        print(dateFormatter.string(from: Date()))
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        dateFormatter.dateStyle = .short
+        print(dateFormatter.string(from: Date()))
+        
+        dateFormatter.dateStyle = .medium
+        print(dateFormatter.string(from: Date()))
+        
+        dateFormatter.dateStyle = .long
+        print(dateFormatter.string(from: Date()))
         
     }
-
-
+    
+    private func setTimeStyle(){
+        
+        dateFormatter.timeStyle = .full
+        print(dateFormatter.string(from: Date()))
+        
+        dateFormatter.timeStyle = .short
+        print(dateFormatter.string(from: Date()))
+        
+        dateFormatter.timeStyle = .medium
+        print(dateFormatter.string(from: Date()))
+        
+        dateFormatter.timeStyle = .long
+        print(dateFormatter.string(from: Date()))
+        
+    }    
 }
 
