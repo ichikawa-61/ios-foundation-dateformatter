013ee41d5e28da293a5baf6f77729d6522476be9
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-05-19 10:53:25 +0900

    first commit

---
+++ ios-foundation-dateformatter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-foundation-dateformatter
+//
+//  Created by Manami Ichikawa on 5/19/17.
+//  Copyright © 2017 Manami Ichikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
