2bc363bc0f48d8a2ec5486cd25ed2827c7eb3b06
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-05-19 15:26:59 +0900

    edit README

--- README.md
+++ README.md
@@ -2,9 +2,27 @@
 
 ## 概要
 
+日時をDate型からString型、または、String型からDate型へ変換するためのフォーマットをインスタンスを生成するクラス
+
 ## 関連クラス
 
+Formatter
+
+## 主要プロパティ
+| プロパティ | 説明　| サンプル  |
+|---|---|---|
+| locale | ロケールを設定 |dateFormater.locale = Locale(identifier: "ja_JP")|
+| dateStyle | 日付の形式を設定する | dateFormater.dateStyle = .full |
+| timeStyle | 時間の形式を設定する | dateFormater.timeStyle = .medium |
+| dateFormat | 日付の書式を設定する | dateFormater.dateFormat = "yyyy/MM/dd HH:mm:ss"|
+
+
+
 ## 主要メソッド
+| メソッド | 説明 | サンプル　|
+|---|---|---|
+| date | 文字列を日付型に変換する | dateFormater.date(from: "2016/10/3 12:12:12") |
+| string | 日付型を文字列に変換する | dateFormater.string(from: Date())) |
 
 ## フレームワーク
 Foundation.framework
--- ios-foundation-dateformatter/ViewController.swift
+++ ios-foundation-dateformatter/ViewController.swift
@@ -21,46 +21,62 @@ class ViewController: UIViewController {
         setTimeStyle()
     }
     
+    //フォーマットの形式を指定する
     private func setDateFormat(){
     
         dateFormatter.dateFormat = "yy/MM/dd HH:mm:ss"
     }
     
+    //日本時間にセット
     private func setLocale(){
         
         dateFormatter.locale = Locale(identifier: "ja_JP")
     }
     
+    //String型へ変換
+    private func convertDateToString(){
+        dateFormatter.string(from: Date())
+    }
+    
+    //Date型へ変換
+    private func convertStringToDate(){
+        
+        let date = dateFormatter.date(from: "2017/05/16 11:11:11")
+        print(date ?? "covert failure")
+    }
+    
+    
+    
     private func testDateFormatterStyle(){
         
         dateFormatter.dateStyle = .full
-        print(dateFormatter.string(from: Date()))
-
+        print(dateFormatter.string(from: Date()))    //2017年5月19日金曜日
+        
         dateFormatter.dateStyle = .short
-        print(dateFormatter.string(from: Date()))
+        print(dateFormatter.string(from: Date()))    //2017/05/19
         
         dateFormatter.dateStyle = .medium
-        print(dateFormatter.string(from: Date()))
+        print(dateFormatter.string(from: Date()))    //2017/05/19
         
         dateFormatter.dateStyle = .long
-        print(dateFormatter.string(from: Date()))
+        print(dateFormatter.string(from: Date()))    //2017年5月19日
         
     }
     
     private func setTimeStyle(){
         
         dateFormatter.timeStyle = .full
-        print(dateFormatter.string(from: Date()))
+        print(dateFormatter.string(from: Date()))    //2017年5月19日 14時54分35秒 日本標準時
         
         dateFormatter.timeStyle = .short
-        print(dateFormatter.string(from: Date()))
+        print(dateFormatter.string(from: Date()))    //2017年5月19日 14:54
         
         dateFormatter.timeStyle = .medium
-        print(dateFormatter.string(from: Date()))
+        print(dateFormatter.string(from: Date()))    //2017年5月19日 14:54:35
         
         dateFormatter.timeStyle = .long
-        print(dateFormatter.string(from: Date()))
+        print(dateFormatter.string(from: Date()))    //2017年5月19日 14:54:35 JST
         
-    }    
+    }
 }
 
